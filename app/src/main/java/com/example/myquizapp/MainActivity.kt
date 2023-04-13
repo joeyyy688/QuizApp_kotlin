8d508bd6c0a32aec85e8bca6b05d0a2b3df86811
joeyyy688 <edinjoey@gmail.com>
2023-04-13 13:09:21 +0000

    Initial commit

---
+++ app/src/main/java/com/example/myquizapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myquizapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val coordinatorLayout = findViewById<LinearLayout>(R.id.mainLayout);
+
+        val startButton = findViewById<Button>(R.id.startBtn)
+        val textInput = findViewById<EditText>(R.id.nameTextInput)
+
+        startButton.setOnClickListener {
+            if (textInput.text.isNotEmpty()){
+                // Navigate to quiz screen
+            }else {
+                val snackBar = Snackbar.make(coordinatorLayout, "Please make sure to enter your name before starting", Snackbar.LENGTH_LONG)
+                snackBar.show()
+
+            }
+        }
+    }
+
+
+}
